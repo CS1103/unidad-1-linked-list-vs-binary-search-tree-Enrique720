c28ae18833bbf9b4c80fe7c8f112d6e7abf5bb03
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-04-15 23:55:01 -0500

    Linkedlist

--- LinkedList.h
+++ LinkedList.h
@@ -1,16 +1,15 @@
-//
-// Created by ruben on 4/5/19.
-//
 
 #ifndef LINKEDLISTVSBINARYTREE_LINKEDLIST_H
 #define LINKEDLISTVSBINARYTREE_LINKEDLIST_H
 #include "Location.h"
-
+#include <iostream>
+#include <fstream>
 namespace UTEC {
 
     struct Node {
         Location data;
         Node* next;
+        Node(Location _data): data{_data}, next{nullptr} {}
     };
 
     class LinkedList {
@@ -26,8 +25,8 @@ namespace UTEC {
         void add_head(const Location& data);
         void add_tail(const Location& data);
         void print();
-        void insert(Node* position, const Location& data);
-        Node* search(std::string position_id);
+        void insert(int position, Location data);
+        Node* search(int position_id);
     };
     void load_locations(LinkedList* linked_list, std::string file_name);
 }
