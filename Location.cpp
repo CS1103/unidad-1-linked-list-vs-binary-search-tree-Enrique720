990708277800b1fad969de3d3215fb32e85c6c75
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-04-15 23:54:23 -0500

    metodos

--- Location.cpp
+++ Location.cpp
@@ -1,7 +1,11 @@
-//
-// Created by ruben on 4/5/19.
-//
-
 #include "Location.h"
-
+Location::Location(int position_id, std::string state_code, std::string county, double latitude, double longitude,
+                   std::string line, std::string construction): position_id{ position_id} , state_code{ state_code},county{county} ,
+                   latitude{latitude},longitude{longitude}{}
+void Location::imprimir_Location() {
+    std::cout << position_id << " " << state_code << " " << county << " " << latitude << " " << longitude << " " << line << " " << construction << std::endl;
+}
+int Location::get_pos() {
+    return position_id;
+}
 // Defina los metodos que crea necesario
