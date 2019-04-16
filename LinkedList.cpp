86a4713efce286d2102361b22005ed0f7b88dcf6
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-04-15 23:55:59 -0500

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,12 +1,7 @@
-//
-// Created by ruben on 4/5/19.
-//
 
 #include "LinkedList.h"
 
-UTEC::LinkedList::LinkedList() {
-
-}
+UTEC::LinkedList::LinkedList(): head{nullptr}, tail{nullptr}{}
 
 UTEC::LinkedList::~LinkedList() {
 
@@ -53,5 +48,65 @@ void UTEC::LinkedList::add_tail(const Location& data) {
     } else {
         tail->next = temp;
         tail = temp;
+    }
+}
+void UTEC::LinkedList::print() {
+    Node* temp = head;
+    temp->data.imprimir_Location();
+}
+UTEC::Node * UTEC::LinkedList::search(int position_id) {
+    if(is_empty())
+        return nullptr;
+    else{
+        Node* temp = head;
+        while(temp != nullptr){
+            if(temp->data.get_pos() == position_id ){
+                return temp;
+            }
+            else{
+                temp = temp->next;
+            }
+        }
+    }
+}
+void UTEC::LinkedList::insert(int position, Location data) {
+    Node* nuevo = new Node(data);
+    Node* temp = head;
+    if(position < size() ) {
+        for (int i = 0; i < position; i++) {
+            temp = temp->next;
+        }
+        nuevo->next = temp->next;
+        temp->next = nuevo;
+    }
+}
+void UTEC::load_locations(UTEC::LinkedList *linked_list, std::string file_name) {
+    std::fstream file(file_name, std::ios::in);
+    std::string local;
+    getline(file,local);
+    int position_id;
+    std::string state_code;
+    std::string county;
+    double latitude;
+    double longitude;
+    std::string line;
+    std::string construction;
+    for(int i  = 0 ; i < 36336; i++ ) {
+        getline(file, local, ',');
+        position_id = atoi(local.c_str());
+        getline(file, local, ',');
+        state_code = local;
+        getline(file, local, ',');
+        county = local;
+        getline(file, local, ',');
+        latitude = atoi(local.c_str());
+        getline(file, local, ',');
+        longitude = atoi(local.c_str());
+        getline(file, local, ',');
+        line = local;
+        getline(file, local, ',');
+        construction = local;
+        Location a(position_id, state_code, county, latitude, longitude, line, construction);
+        linked_list->add_head(a);
     }
 }
